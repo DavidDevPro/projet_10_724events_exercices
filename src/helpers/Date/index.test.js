41decaf185e0721ad975994f671482a577811580
David Changea <14ir155@gmail.com>
2024-02-27 11:43:38 +0100

    first commit

---
+++ src/helpers/Date/index.test.js
@@ -0,0 +1,19 @@
+import { getMonth } from "./index";
+
+/**
+ test unitaire getMonth 
+ */
+
+describe("Date helper", () => {
+    describe("When getMonth is called", () => {
+        it("the function return janvier for 2022-01-01 as date", () => {
+            const date = new Date("2022-01-01T00:00:00"); // Définir le temps à 00:00:00 pour éviter les variations dues au décalage horaire
+            expect(getMonth(date)).toBe("janvier");
+        });
+        it("the function return juillet for 2022-07-08 as date", () => {
+            const date = new Date("2022-07-08T00:00:00"); // Définir le temps à 00:00:00 pour éviter les variations dues au décalage horaire
+            expect(getMonth(date)).toBe("juillet");
+        });
+    });
+})
+

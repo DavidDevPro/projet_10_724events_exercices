46a2c4a14101f64b3c9960530f1d57cff14df43d
David Changea <14ir155@gmail.com>
2024-02-27 12:35:46 +0100

    commentaire dans helpers/date/index.test.je de la variable 'MONTHS' dans son état de base

--- src/helpers/Date/index.js
+++ src/helpers/Date/index.js
@@ -1,3 +1,20 @@
+{/* variable de base :
+export const MONTHS = {
+  1: "janvier",
+  2: "février",
+  3: "mars",
+  4: "avril",
+  5: "mai",
+  6: "juin",
+  7: "juillet",
+  8: "août",
+  9: "septembre",
+  10: "octobre",
+  11: "novembre",
+  12: "décembre",
+}; */}
+
+
 export const MONTHS = {
   0: "janvier",
   1: "février",
